a5b57d3e17408fe8ed63d9a6f21f76dd85a5706f
kjkostlan <kkostla1@jhu.edu>
2022-10-04 15:27:55 -0700

    Better fps and frame handling/errors (still not perfect).

--- TapeyTeapots/pandawrapper.py
+++ TapeyTeapots/pandawrapper.py
@@ -13,18 +13,22 @@ from TapeyTeapots.yuckymutate import scenesync, mousekey
 import reload
 import numpy as np
 
+log = None # Set the log to log the panda3d calls.
+
 class App:
 
-    def __init__(self, initial_state, every_frame_fn, panda_config=None):
+    def __init__(self, initial_state, every_frame_fn, panda_config=None, log=None):
 
         self.appstate = initial_state
         self.every_frame_fn = every_frame_fn
         self.appstate['elapsed_frames'] = 0
         self.old_appstate = {}
+        self.last_time = time.time()
         self.showbase = showbase = ShowBase()
         self.panda_stuff_that_mutates = {}
+        self.log = log
         showbase.disable_mouse() # Disables the default mouse camara control.
-        
+
         self.task_mgr = showbase.task_mgr
         self.mouse_watcher = showbase.mouseWatcherNode
         #self.gui_mouse_watcher = gui_mouse_watcher
@@ -48,6 +52,8 @@ class App:
         filters = CommonFilters(base.win, base.cam)
         filters.setSrgbEncode()
 
+        if log is not None:
+            log.append(['setup', self, None, '<init stuff>'])
         #https://docs.panda3d.org/1.10/python/programming/configuration/accessing-config-vars-in-a-program
         if panda_config is not None:
             print('Custom panda config (warning: will last until app restart):', panda_config)
@@ -56,6 +62,8 @@ class App:
                 #print('Specified in config file: ', var_ob.getValue())
                 var_ob.setValue(str(v))
                 #print('Value we will use: ', var_ob.getValue())
+            if log is not None:
+                log.append(['setup_config', self, None, panda_config])
 
         # https://discourse.panda3d.org/t/userexit-without-kill-the-python-interpreter/10683
         try:
@@ -65,6 +73,17 @@ class App:
             showbase.destroy()
 
     def _every_frame(self, task):
+        # task.cont has internal steps that takes time.
+        # Consider this point the beginning of the frame. We sleep before starting the next frame
+        # if the last frame went too fast for our fps.
+        # (the fps counter seems bugged and is too high!?)
+        t0 = self.last_time
+        t1 = time.time()
+        self.last_time = t1
+        fps = self.appstate.get('fps',30)
+        if fps>0:
+            if t1-t0<1.0/fps:
+                time.sleep(1.0/fps-(t1-t0))
 
         self.screen_state = [base.win.getProperties().getXSize(), base.win.getProperties().getYSize()]
         mousekey.update_mouse_pos(self.mouse_state, self.screen_state, stretch_to_screen=self.appstate.get('stretch_to_screen',False))
@@ -74,12 +93,12 @@ class App:
             ui = {'mouse':self.mouse_state,'keyboard':self.key_state,'click':mouse_clicks,'type':key_clicks, 'screen':self.screen_state}
             appstate1 = self.every_frame_fn(self.appstate, ui)
         except Exception:
-            print('Every Frame Error:')
+            print('Every Frame Error (fps limited to 4 when printing errors):')
             print(traceback.format_exc())
             time.sleep(0.25)
-            appstate1 = None
+            appstate1 = self.appstate
         if appstate1 is None or type(appstate1) is not dict or len(appstate1.keys())==0:
-            print('Every Frame returns None or empty app-state, which will be not used.')
+            print('Every Frame returns None or empty app-state, which will be not used (fps limited to 4 when printing errors).')
             appstate1 = self.appstate
             time.sleep(0.25)
 
@@ -94,7 +113,7 @@ class App:
         oldstate = self.old_appstate
         newstate = self.appstate
         try:
-            scenesync.sync(self.old_appstate, self.appstate, self.screen_state, self.panda_stuff_that_mutates, self.pivot)
+            scenesync.sync(self.old_appstate, self.appstate, self.screen_state, self.panda_stuff_that_mutates, self.pivot, self.log)
         except Exception:
             print('Appstate synchronization error:')
             print(traceback.format_exc())
